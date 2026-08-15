ad4017ce6dd9602c442942e40e5470c00e745e73
Aryan <aryan.chhattani@gmail.com>
2026-08-15 10:50:19 -0500

    Add FeatureExtractor and Fetcher classes with feature computation logic

---
+++ App/Fetcher.java
@@ -0,0 +1,15 @@
+/**
+ * Fetcher
+ */
+public class Fetcher {
+    public static class Bar {
+        public double close;
+
+        public Bar() {
+        }
+
+        public Bar(double close) {
+            this.close = close;
+        }
+    }
+}
